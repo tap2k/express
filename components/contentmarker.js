8dd51094176da1309925cd603fdc3648183d9a6f
Tapan Parikh <tparikh@gmail.com>
2024-08-28 11:45:47 -0400

    fix popup

--- components/contentmarker.js
+++ components/contentmarker.js
@@ -38,7 +38,7 @@ const ContentMarker = forwardRef(function ContentMarker(props, fwdRef)
 
   return (
     <>
-      <Marker position={[contentItem.lat, contentItem.long]} icon={icon} ref={(el)=> {markerRef.current = el; if (fwdRef) fwdRef(el);}} eventHandlers={eventHandlers} draggable={privateID ? true : false} >
+      <Marker position={[contentItem.lat, contentItem.long]} icon={icon} ref={(el)=> {markerRef.current = el; if (fwdRef) fwdRef(el);}} eventHandlers={eventHandlers} draggable={privateID ? true : false}>
         { contentItem.title && <Tooltip>
             <div style={{textAlign: 'center'}}><b>{contentItem.title}</b></div>
           </Tooltip>    
