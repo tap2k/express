923647b624ac54c69a2b6c664aa00b195bf2e6ed
Tapan Parikh <tparikh@gmail.com>
2024-09-16 11:36:58 -0400

    hide title

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,43 @@
-import nookies from 'nookies';
+import nookies, { destroyCookie } from 'nookies';
+import getUser from "../hooks/getuser";
+import getMyChannels from "../hooks/getmychannels";
+import getPublicChannels from "../hooks/getpublicchannels";
+import { RecorderWrapper } from '../components/recorderstyles';
+import BannerTwo from '../components/bannertwo';
+import MyReels from "../components/myreels";
 
-export default function Home( ) {
+export default ({ user, jwt, channels }) => {
+
+  return (
+    <RecorderWrapper>
+      <BannerTwo jwt={jwt} />
+      <MyReels channels={channels} user={user} jwt={jwt} />
+    </RecorderWrapper>
+  )
 }
 
 export const getServerSideProps = async (ctx) => {
-    //const cookies = nookies.get(ctx);
-    //if (cookies?.jwt) 
+  const cookies = nookies.get(ctx);    
+  if (!cookies?.jwt)
+  {
+    let channels = await getPublicChannels();
     return {
-      redirect: { permanent: false, destination: '/myreels' }
+      props: { channels }
     }
-    
-    /*return {
-        redirect: { permanent: false, destination: '/make' }
+  }
+
+  let user = await getUser(cookies.jwt);
+  let channels = await getMyChannels(cookies.jwt);
+
+  if (!user)
+  { 
+    destroyCookie(ctx, 'jwt', { path: '/' });
+    return {
+      redirect: { permanent: false, destination: '/' }
     }
-    
-    return { props: {} };*/
+  }
+
+  return {
+    props: { user, jwt: cookies.jwt, channels }
+  };
 }
