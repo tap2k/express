a22b68806c3db8c12c41f9c85277929d7cef95a2
Tapan Parikh <tparikh@gmail.com>
2024-10-01 10:21:32 -0400

    thumbs

--- components/content.js
+++ components/content.js
@@ -77,8 +77,8 @@ export function getMediaInfo(contentItem, thumbnail) {
     if (type === 'video/ogg' || type === 'video/mp4' || type === 'video/webm')
       videotype = type;
 
-    if (type.startsWith("image") && thumbnail && contentItem.mediafile?.formats?.large?.url)
-      url = getMediaURL() + contentItem.mediafile.formats.large.url;
+    //if (type.startsWith("image") && thumbnail && contentItem.mediafile?.formats?.large?.url)
+    //url = getMediaURL() + contentItem.mediafile.formats.large.url;
 
     if (type && (type.startsWith("video") || type.startsWith("image") || type.startsWith("audio")))
       return { url, type, videotype };
